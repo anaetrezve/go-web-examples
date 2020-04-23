2fbde5ba39a31ae9236711292afcdeed1d820aa0
Anaet <anaethoss@gmail.com>
2020-04-23 13:41:54 +0600

    forms

--- forms/main.go
+++ forms/main.go
@@ -6,6 +6,12 @@ import (
 	"text/template"
 )
 
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
 func main() {
 	bp, _ := filepath.Abs("./")
 	fp := filepath.Join(bp, "forms")
@@ -13,7 +19,26 @@ func main() {
 	tmpl := template.Must(template.ParseFiles(filepath.Join(fp, "index.gohtml")))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		formData := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		resp := struct {
+			Success  bool
+			FormData ContactDetails
+		}{
+			Success:  true,
+			FormData: formData,
+		}
+
+		tmpl.Execute(w, resp)
 	})
 
 	http.ListenAndServe(":5000", nil)
